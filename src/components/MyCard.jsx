d65bb997d31345c50815d1ae56f39623a8f1480a
Cristian Venegas <cristian.venegas@materoceano.com>
2022-09-29 19:09:17 -0300

    fix : fetch request

--- src/components/MyCard.jsx
+++ src/components/MyCard.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react'
-import { AppContext } from '../context/AppContext'
+
 
 const MyCard = () => {
-  const {data, setData} = useContext(AppContext)
+ 
   return (
-    <div>{data}</div>
+    <div></div>
   )
 }
 
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,37 @@
 import { createContext, useState } from "react";
+import fotos from '../data/fotos.json'
+export const AppContext = createContext();
 
-export const AppContext = createContext()
+const AppProvider = ({ children }) => {
+  const [data, setData] = useState([]);
 
-import React from 'react'
+  const APIKEY = "563492ad6f917000010000011dec5a491d6940fdbaabae66f4986fd6";
+  const ENDPOINT = "https://api.pexels.com/v1/search?query=people";
 
-const AppProvider = ({children}) => {
-  const [data, setData] = useState("Cristian")
+
+  const getData = async () => {
+    const response = await fetch("https://api.pexels.com/v1/search?query=people",
+    {method: "GET",
+      headers: {
+        Authorization:
+          "563492ad6f917000010000011dec5a491d6940fdbaabae66f4986fd6",
+      },
+    })
+    const data = await response.json()
+    console.log(data.photos)
+
+  };
+
+  getData()
+
+
+
+
   return (
-    <AppContext.Provider value={{data, setData}}>
+    <AppContext.Provider value={{ data, setData }}>
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 
-export default AppProvider
+export default AppProvider;
