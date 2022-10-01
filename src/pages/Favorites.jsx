1fe4726cb4d91d1c3453949ac65ef4d38d98c4fc
Cristian Venegas <cristian.venegas@materoceano.com>
2022-09-30 23:41:25 -0300

    first upload

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,11 +2,14 @@ import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 
 const Favorites = () => {
-  const {likes} = useContext(AppContext)
+  const {data} = useContext(AppContext)
+
+  const dataFiltrada = data.filter(item => item.liked ===true)
+
   return (
     <div className="p-2">
-    {/* <MyContainer data = {likes}/> */}
-    <h1>Favorites</h1>
+    {/* <MyContainer data = {dataFiltrada}/> */}
+    {dataFiltrada.map(item => <p key={item.id}>{item.id} </p> )}
 
     
   </div>
